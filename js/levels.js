410d707ef5fff0a0b9e15c3d24841551c004a0e5
Hagar Shilo <strayblues@gmail.com>
2017-10-28 21:02:23 +0300

    Write level detection function

--- js/levels.js
+++ js/levels.js
@@ -1,3 +1,24 @@
+// L1 distance
+function colorDifference(a, b){
+  var redDiff = Math.abs(a[0] - b[0]),
+      greenDiff = Math.abs(a[1] - b[1]),
+      blueDiff = Math.abs(a[2] - b[2]);
+  // values between 0 and 765
+  var diff = redDiff+greenDiff+blueDiff;
+
+  return diff;
+}
+
+
+function detectLevel(colorList){
+  var a = colorDifference(colorList[0], colorList[1]);
+  var b = colorDifference(colorList[1], colorList[2]);
+  var c = colorDifference(colorList[0], colorList[2]);
+
+  var smallestDiff = Math.min(a, b, c);
+
+  return smallestDiff;
+}
 
 // For first-timers
 function practiceColor(){
@@ -7,7 +28,7 @@ function practiceColor(){
   // Pick a 'blue' value of 0 or 255
   var b = (Math.floor(Math.random() * 2) == 0) ? 0 : 255;
 
-  return "rgb(" + r + ", " + g + ", " + b + ")";
+  return [r, g, b];
 }
 
 function generateRandomColors(num){
@@ -15,6 +36,7 @@ function generateRandomColors(num){
     for(var i=0; i<num; i++){
       arr.push(randomColor());
     }
+    console.log(detectLevel(arr));
   return arr;
 }
 
@@ -28,7 +50,6 @@ function randomColor(){
   var b = Math.floor(Math.random() * 256);
 
   return [r, g, b];
-
 }
 
 
